b334b0763145d93f36599f6a99d75806525e46c2
ra-kesh <iamrakeshkumar@pm.me>
2025-03-24 12:30:41 +0530

    iniitalised supasbe client successfully

--- src/app/(dashboard)/projects/page.tsx
+++ src/app/(dashboard)/projects/page.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+"use client";
+
+import { createSupabaseClient } from "@/supabase-utils/client";
+import React, { useEffect } from "react";
 
 const Projects = () => {
+  useEffect(() => {
+    const supabase = createSupabaseClient();
+    supabase.storage
+      .listBuckets()
+      .then((result) => console.log("Bucket List", result));
+  });
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">all projects</div>
   );
